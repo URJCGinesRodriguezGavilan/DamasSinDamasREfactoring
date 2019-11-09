7b32731dc7fa12fb0fc60d6a6648ae336f1002de
URJCGinesRodriguezGavilan <g.rodriguezg.2019@alumnos.ujrc.es>
2019-11-09 18:02:49 +0100

    Messages in the views. New Class MessageView

---
+++ 1.ADCS.damas-master/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/MessageView.java
@@ -0,0 +1,30 @@
+package es.urjccode.mastercloudapps.adcs.draughts.views;
+
+import es.urjccode.mastercloudapps.adcs.draughts.utils.Console;
+
+enum MessageView{
+
+    TITTLE("Draughts"),
+    PLAY_AGAIN("¿Queréis jugar otra"),
+    YOU_LOSE("Derrota!!! No puedes mover tus fichas!!!");
+
+    private String message;
+
+    private static Console console = new Console();
+
+    private MessageView( String message){
+        this.message =message;
+    }
+
+    public void write(){
+        MessageView.console.write(this.message);
+    }
+
+    public void writeln(){
+        MessageView.console.writeln(this.message);
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
--- 1.ADCS.damas-master/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/ResumeView.java
+++ 1.ADCS.damas-master/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/ResumeView.java
@@ -4,8 +4,6 @@ import es.urjccode.mastercloudapps.adcs.draughts.controllers.ResumeController;
 import es.urjccode.mastercloudapps.adcs.draughts.utils.YesNoDialog;
 
 public class ResumeView extends SubView {
-
-    private static final String MESSAGE = "¿Queréis jugar otra";
     
     private YesNoDialog yesNoDialog;
 
@@ -15,7 +13,7 @@ public class ResumeView extends SubView {
     }
 
     public void interact(ResumeController resumeController) {
-        if (this.yesNoDialog.read(ResumeView.MESSAGE)){
+        if (this.yesNoDialog.read(MessageView.PLAY_AGAIN.getMessage())){
             resumeController.reset();
         } else {
             resumeController.next();
